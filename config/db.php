60c5f7a236aa68673b5e1928a43046a85030a162
gabrielrmodesto <gabrielrmodesto5@gmail.com>
2019-08-24 17:16:20 -0300

    Sistema fincanceiro

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'sistema_financeiro',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
