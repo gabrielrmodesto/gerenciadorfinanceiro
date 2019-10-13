dc4d300e79039d5b53ff536c164a7ec16ac5f921
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2019-10-13 17:42:57 -0300

    Organizando para nao subir arquivos de configuracao

---
+++ config/dbexample.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => '',
+        'host' => '',
+        'database' => '',
+        'username' => '',
+        'password' => '',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
